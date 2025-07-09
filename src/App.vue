cb3f53fa8ffadb0305d9043caf1c35cd6de14d2f
FinFox1 <mail@driller.biz>
2025-07-10 00:39:31 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <NcAppNavigation>
+      <template #list>
+        <NcAppNavigationItem
+          :title="t('nextcloud_chat', 'Rooms')"
+          icon="icon-comment"
+          :to="{ path: '/' }"
+        />
+        <NcAppNavigationItem
+          :title="t('nextcloud_chat', 'Settings')"
+          icon="icon-settings"
+          :to="{ path: '/settings' }"
+        />
+      </template>
+    </NcAppNavigation>
+
+    <NcAppContent>
+      <router-view />
+    </NcAppContent>
+  </div>
+</template>
+
+<script>
+import {
+  NcAppNavigation,
+  NcAppNavigationItem,
+  NcAppContent
+} from '@nextcloud/vue'
+
+export default {
+  name: 'NextcloudChatApp',
+  components: {
+    NcAppNavigation,
+    NcAppNavigationItem,
+    NcAppContent
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  height: 100vh;
+  display: flex;
+}
+</style>
