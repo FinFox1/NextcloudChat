93851402ab61bf3ec20ff1f8de181c7ecd3d59c9
FinFox1 <mail@driller.biz>
2025-07-10 00:54:39 +0200

    Create index.php

---
+++ templates/index.php
@@ -0,0 +1,13 @@
+<?php
+script('nextcloud_chat', 'nextcloud_chat-main');
+style('nextcloud_chat', 'main');
+?>
+
+<div id="content" class="app-nextcloud_chat">
+    <div id="app-navigation" class="app-navigation">
+        <!-- Vue router will handle navigation -->
+    </div>
+    <div id="app-content" class="app-content">
+        <!-- Vue app will be mounted here -->
+    </div>
+</div>
